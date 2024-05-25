91a25580569b0cb86d74f64aa74fe598f805c02e
Nasim <nasimhelal123>
2024-05-25 15:54:11 +0600

    Message page updated

--- src/app/(main)/department/(message)/messages/page.tsx
+++ src/app/(main)/department/(message)/messages/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Bird,
   Book,
@@ -37,117 +39,106 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
+import DropDown, { List } from "@/components/common/DropDown";
+import { useState } from "react";
+import { P } from "@/components/common/TypoGraphy";
+
+let messageToDropDownList: List[] = [
+  {
+    title: "All Students",
+    value: "all-students",
+  },
+  {
+    title: "All Teachers",
+    value: "all-teachers",
+  },
+  {
+    title: "Specific Student",
+    value: "specific-student",
+  },
+  {
+    title: "Specific Teacher",
+    value: "specific-teacher",
+  },
+  {
+    title: "Students By Class",
+    value: "students-by-class",
+  },
+];
+
+let teacherList: List[] = [
+  {
+    title: "X",
+    value: "x",
+  },
+  { title: "Y", value: "y" },
+];
 
 export default function MessagePage() {
+  const [receiver, setReceiver] = useState("");
   return (
-    <main className="grid flex-1 gap-4 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-3">
+    <main className="grid flex-1 gap-4 overflow-auto p-4 md:grid-cols-2 lg:grid-cols-3 min-h-[85vh]">
       <div
         className="relative hidden flex-col items-start gap-8 md:flex"
         x-chunk="dashboard-03-chunk-0"
       >
         <form className="grid w-full items-start gap-6">
           <fieldset className="grid gap-6 rounded-lg border p-4">
-            <legend className="-ml-1 px-1 text-sm font-medium">Settings</legend>
+            <legend className="-ml-1 px-1 text-sm font-medium">Message</legend>
             <div className="grid gap-3">
-              <Label htmlFor="model">Model</Label>
-              <Select>
-                <SelectTrigger
-                  id="model"
-                  className="items-start [&_[data-description]]:hidden"
-                >
-                  <SelectValue placeholder="Select a model" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="genesis">
-                    <div className="flex items-start gap-3 text-muted-foreground">
-                      <Rabbit className="size-5" />
-                      <div className="grid gap-0.5">
-                        <p>
-                          Neural{" "}
-                          <span className="font-medium text-foreground">
-                            Genesis
-                          </span>
-                        </p>
-                        <p className="text-xs" data-description>
-                          Our fastest model for general use cases.
-                        </p>
-                      </div>
-                    </div>
-                  </SelectItem>
-                  <SelectItem value="explorer">
-                    <div className="flex items-start gap-3 text-muted-foreground">
-                      <Bird className="size-5" />
-                      <div className="grid gap-0.5">
-                        <p>
-                          Neural{" "}
-                          <span className="font-medium text-foreground">
-                            Explorer
-                          </span>
-                        </p>
-                        <p className="text-xs" data-description>
-                          Performance and speed for efficiency.
-                        </p>
-                      </div>
-                    </div>
-                  </SelectItem>
-                  <SelectItem value="quantum">
-                    <div className="flex items-start gap-3 text-muted-foreground">
-                      <Turtle className="size-5" />
-                      <div className="grid gap-0.5">
-                        <p>
-                          Neural{" "}
-                          <span className="font-medium text-foreground">
-                            Quantum
-                          </span>
-                        </p>
-                        <p className="text-xs" data-description>
-                          The most powerful model for complex computations.
-                        </p>
-                      </div>
-                    </div>
-                  </SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="temperature">Temperature</Label>
-              <Input id="temperature" type="number" placeholder="0.4" />
-            </div>
-            <div className="grid grid-cols-2 gap-4">
-              <div className="grid gap-3">
-                <Label htmlFor="top-p">Top P</Label>
-                <Input id="top-p" type="number" placeholder="0.7" />
-              </div>
-              <div className="grid gap-3">
-                <Label htmlFor="top-k">Top K</Label>
-                <Input id="top-k" type="number" placeholder="0.0" />
-              </div>
-            </div>
-          </fieldset>
-          <fieldset className="grid gap-6 rounded-lg border p-4">
-            <legend className="-ml-1 px-1 text-sm font-medium">Messages</legend>
-            <div className="grid gap-3">
-              <Label htmlFor="role">Role</Label>
-              <Select defaultValue="system">
-                <SelectTrigger>
-                  <SelectValue placeholder="Select a role" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="system">System</SelectItem>
-                  <SelectItem value="user">User</SelectItem>
-                  <SelectItem value="assistant">Assistant</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="content">Content</Label>
-              <Textarea
-                id="content"
-                placeholder="You are a..."
-                className="min-h-[9.5rem]"
+              <Label htmlFor="person">Message to</Label>
+              <DropDown
+                lists={messageToDropDownList}
+                onChange={setReceiver}
+                value={receiver}
+                placeholder="Select a person"
               />
             </div>
           </fieldset>
+          {
+            <fieldset className="grid gap-6 rounded-lg border p-4">
+              <legend className="-ml-1 px-1 text-sm font-medium">
+                More filter
+              </legend>
+
+              {receiver === "specific-teacher" ? (
+                <div className="grid gap-3">
+                  <Label htmlFor="role">Select teacher</Label>
+                  <Select>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a teacher" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="system">System</SelectItem>
+                      <SelectItem value="user">User</SelectItem>
+                      <SelectItem value="assistant">Assistant</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              ) : receiver === "specific-student" ? (
+                <div className="grid gap-3">
+                  <Label htmlFor="role">Search Student</Label>
+                  <Input type="text" placeholder="Type Student's name" />
+                </div>
+              ) : receiver === "students-by-class" ? (
+                <div className="grid gap-3">
+                  <Label htmlFor="role">Search Student</Label>
+                  <Select>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a teacher" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="system">System</SelectItem>
+                      <SelectItem value="user">User</SelectItem>
+                      <SelectItem value="assistant">Assistant</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              ) : (
+                <P text="No need to filter" />
+              )}
+            </fieldset>
+          }
         </form>
       </div>
       <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
